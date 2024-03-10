a72f70cd5ba03caacedc181a18bca9eca4fb4ae8
Crowdbotics <team@crowdbotics.com>
2024-03-10 06:56:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-waterfall-46910",
+  url: "https://delicate-waterfall-46910.botics.co",
+  api: "https://delicate-waterfall-46910.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
